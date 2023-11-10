14a2a616d953b506d632b49aef3ef2d65fbc3f5b
Rasmus Sievers <rascam@posteo.de>
2023-11-10 18:58:44 +0100

    connection with Postgres db

---
+++ apps/server/prisma/prismaSeed.mjs
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+async function seed() {
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "3gY1F@example.com",
+    },
+  })
+}
+
+seed()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
