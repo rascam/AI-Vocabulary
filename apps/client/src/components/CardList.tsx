f645f09aca213f003677c7ec5279b76609c03910
Rasmus Sievers <rascam@posteo.de>
2023-11-18 16:40:41 +0100

    fix card background image

--- apps/client/src/components/CardList.tsx
+++ apps/client/src/components/CardList.tsx
@@ -1,10 +1,16 @@
+import { defaultImg } from '../lib/const'
+
 function CardList(words: any) {
 
   return (
     <div className="">
       {words.map((word: any) => (
-        <div key={word.id} className={`card card${word.bin}`} style={{background: "linear-gradient(#19778d99, #19778d99), url(https://images.unsplash.com/photo-1617975609658-2de247badd36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NjY3ODh8MHwxfHNlYXJjaHw0fHxsYW5ndWFnZSUyMGRpY3Rpb25hcnl8ZW58MXwwfHx8MTY4Nzg5MzkzMHww&ixlib=rb-4.0.3&q=80&w=400); background-size: cover;"}}>
-          <p>{word.srcWord}</p>
+        <div key={word.id} className={`card card${word.bin}`}
+          style={{
+          background: `linear-gradient(#19778d99, #19778d99), url(${word.imgUrl || defaultImg})`,
+          backgroundSize: 'cover',
+        }}>
+        <p>{word.srcWord}</p>
           <div className="thinLine"></div>
           <div className="translation">
             <p>{word.targetWord}</p>
