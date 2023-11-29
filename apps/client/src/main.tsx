c7622bd7561e73d84191edebd1077a8388aa1bd5
Rasmus Sievers <rascam@posteo.de>
2023-11-29 19:06:39 +0100

    routes fix for deployment

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { router } from './routes'
+import { router } from './routes.tsx'
 import { RouterProvider } from 'react-router-dom'
 
 import './assets/App.css'
