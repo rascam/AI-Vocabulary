80aea8f7bef30b5facfc5d3093284f55fa9d0d79
Rasmus Sievers <rascam@posteo.de>
2023-12-05 13:58:03 +0100

    local defaultImage

--- apps/client/src/data/const.ts
+++ apps/client/src/data/const.ts
@@ -1,3 +1,9 @@
-export const defaultImg = 'https://images.unsplash.com/photo-1617975609658-2de247badd36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NjY3ODh8MHwxfHNlYXJjaHw0fHxsYW5ndWFnZSUyMGRpY3Rpb25hcnl8ZW58MXwwfHx8MTY4Nzg5MzkzMHww&ixlib=rb-4.0.3&q=80&w=400'
-export const testUser = 'clphbune40000hvahdgh0ehqp'
-export const BASE_URL = "http://localhost:8080/"
+
+export const BASE_URL: string | undefined = import.meta.env.VITE_BASE_URL
+
+
+export const defaultImg = '../../public/defaultImg.jpg'
+
+
+// export const defaultImg = 'https://images.unsplash.com/photo-1617975609658-2de247badd36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NjY3ODh8MHwxfHNlYXJjaHw0fHxsYW5ndWFnZSUyMGRpY3Rpb25hcnl8ZW58MXwwfHx8MTY4Nzg5MzkzMHww&ixlib=rb-4.0.3&q=80&w=400'
+// export const BASE_URL = "http://localhost:8080/"
