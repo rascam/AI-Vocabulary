ff75606290dd6f2cbcecd0cbd315b64ace07cd1e
Rasmus Sievers <rascam@posteo.de>
2023-12-06 20:05:48 +0100

    const import defaultImage

--- apps/client/src/data/const.ts
+++ apps/client/src/data/const.ts
@@ -3,7 +3,8 @@ export const BASE_URL: string | undefined = import.meta.env.VITE_BASE_URL
 
 export const UNSPLASH_APP_NAME: string | undefined = import.meta.env.UNSPLASH_APP_NAME
 
-export const defaultImg = './public/defaultImg.jpg'
+import imageUrl from '/src/assets/img/defaultImg.jpg'
+export const defaultImg = imageUrl
 
 
 // export const defaultImg = 'https://images.unsplash.com/photo-1617975609658-2de247badd36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NjY3ODh8MHwxfHNlYXJjaHw0fHxsYW5ndWFnZSUyMGRpY3Rpb25hcnl8ZW58MXwwfHx8MTY4Nzg5MzkzMHww&ixlib=rb-4.0.3&q=80&w=400'
